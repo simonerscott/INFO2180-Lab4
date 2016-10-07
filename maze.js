d212b64cff725880baeffeae665dfd4a068c3cc5
simonerscott <rhoniscott@yahoo.com>
2016-10-07 10:26:13 -0500

    Exercise 1 Completed

---
+++ maze.js
@@ -0,0 +1,14 @@
+window.onload=function()
+{
+	var boundary = document.getElementById("boundary1");
+	boundary.addEventListener("mouseover", function()
+	{
+		this.setAttribute("class","boundary youlose");
+	});
+
+
+	
+
+};
+
+
