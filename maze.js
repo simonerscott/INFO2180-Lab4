b57c0d7a299c1767e2026bbf29e61feeb8c63d37
simonerscott <rhoniscott@yahoo.com>
2016-10-07 11:53:10 -0500

    Exercise 3 Completed

--- maze.js
+++ maze.js
@@ -1,22 +1,46 @@
 window.onload=function()
 {
+	var touchBoundary = false;
+
 	//Exercise 1
 	var boundary = document.getElementById("boundary1");
 	boundary.addEventListener("mouseover", function()
 	{
 		this.setAttribute("class","boundary youlose");
+		touchBoundary = true;
 	});
 
 
 	//Exercise 2
 	var otherWalls = document.querySelectorAll(".boundary");
-	var i;
 	for (var i = 0; i < otherWalls.length; i++) 
 	{
 		otherWalls[i].addEventListener("mouseover", function()
 		{
-			this.setAttribute("class","boundary youlose");
+			touchWall(otherWalls);
+			touchBoundary = true;
 		});
+	}
+
+
+	function touchWall(walls)
+	{
+		for (var i = walls.length - 1; i >= 0; i--) 
+		{
+			walls[i].setAttribute("class", "boundary youlose");
+		}
+	}
+
+
+
+	//Exercise 3
+	document.getElementById("end").addEventListener("mouseover",mouseOverEnd);
+	function mouseOverEnd()
+	{
+		if (touchBoundary === false)
+		{
+			alert("You Win");
+		}		
 	}
 
 };
